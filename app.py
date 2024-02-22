fbc1ef2cfe1cd9b49a6846d8cc0abf86fa82f8dc
Thomas <92720389+Thommmas@users.noreply.github.com>
2024-02-22 12:40:21 +0100

    mutli-user addition (wip)

--- app.py
+++ app.py
@@ -1,25 +1,28 @@
 from flask import Flask, jsonify, request, render_template
 
-app = Flask(__name__,template_folder="./templates")
-
-# Initialize data structure to store count
-data = {"count": 0}
+app = Flask(__name__)
+# Store counts for each user
+counts = {}
 
 @app.route("/")
-def home():
+def show_chart():
     return render_template('chart.html')
 
 @app.route("/increment", methods=["GET", "POST"])
 def increment():
     if request.method == "POST":
-        data["count"] += 1
-        return jsonify({"count": data["count"]})
+        username = request.json.get('username')
+        if username in counts:
+            counts[username] += 1
+        else:
+            counts[username] = 1
+        return jsonify({username: counts[username]})
     else:
         return render_template('increment.html')
 
 @app.route("/data", methods=["GET"])
-def get_data():
-    return jsonify(data)
+def data():
+    return jsonify(counts)
 
 if __name__ == "__main__":
     app.run(debug=True)
---
+++ run.bat
@@ -0,0 +1,3 @@
+CALL .venv\Scripts\activate
+set FLASK_APP=app.py
+flask run --host=0.0.0.0
